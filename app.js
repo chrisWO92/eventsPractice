2cd31cb9b4563750e9917d0524792dd94f5a0f80
cristian <chrisworkingonline@gmail.com>
2022-01-19 16:29:54 -0300

    commit

--- app.js
+++ app.js
@@ -3,26 +3,38 @@ const studentEmail = document.querySelector(".student-email");
 const subject = document.querySelector(".subject");
 const submit = document.querySelector(".submit");
 
-
-submit.addEventListener("click", ()=> {
-    
+let validateName = () => {
     if (studentName.value.length == 0) {
         alert("no ha ingresado ningún nombre");
     }
 
-    if (studentName.value.length <= 4){
+    else if (studentName.value.length <= 4 && studentName.value.length != 0){
         alert("El nombre es muy corto, debe tener más de 4 caracteres");
     }
     else if (studentName.value.length >= 40){
         alert("El nombre es muy largo, debe tener menos de 40 caracteres");
     }
     
+}
+
+let validateEmail = () => {        
     if (studentEmail.value == "") {
         alert("no ha ingresado ningún email");
     }
+}
+
+let validateSubject = () => {        
     if (subject.value == "") {
         alert("no ha ingresado ningún subject");
     }
+}
+
+submit.addEventListener("click", ()=> {
+    
+    validateName();
+    validateEmail();
+    validateSubject();
+
 })
 
 
