13c2df5533e992d2dbfdcdd82ae209ccdbf87ca3
cristian <chrisworkingonline@gmail.com>
2022-01-19 12:53:50 -0300

    morning commit

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Problema A
 
 Cofla perdió 2 materias, por lo que tendrá que enviar un formulario a las mesas de examen para poder intentar aprobarlas.
 
-    -Contener nombre, mail yh materia adeudada.
+    -Contener nombre, mail y materia adeudada.
     -Validar mails y nombres
     -Enviar al servidor de manera pulida
--- app.js
+++ app.js
@@ -1,2 +1,28 @@
+const studentName = document.querySelector(".student-name");
+const studentEmail = document.querySelector(".student-email");
+const subject = document.querySelector(".subject");
+const submit = document.querySelector(".submit");
+
+
+submit.addEventListener("click", ()=> {
+    
+    if (studentName.value.length == 0) {
+        alert("no ha ingresado ningún nombre");
+    }
+
+    if (studentName.value.length <= 4){
+        alert("El nombre es muy corto, debe tener más de 4 caracteres");
+    }
+    else if (studentName.value.length >= 40){
+        alert("El nombre es muy largo, debe tener menos de 40 caracteres");
+    }
+    
+    if (studentEmail.value == "") {
+        alert("no ha ingresado ningún email");
+    }
+    if (subject.value == "") {
+        alert("no ha ingresado ningún subject");
+    }
+})
 
 
